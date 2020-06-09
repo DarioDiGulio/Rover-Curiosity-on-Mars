c6205124301efc3185ec24a1a31f657555d823c1
Dario Di Gulio <ddigulio@technisys.com>
2020-06-09 12:48:21 -0300

    correcciones

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import "./styles/index.scss";
 function App() {
   return (
     <Layout
-      title="
-    Images taken by the Rover Curiosity on Mars"
+      title="Images taken by the Rover Curiosity on Mars"
       footer="Dario Di Gulio - Technisys exercise"
     >
       <Slider />
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,64 +1,58 @@
 import React from "react";
-import NexsStep from "./NexStep";
+import IconStep from "./IconStep";
 import SliderStep from "./SliderStep";
-import Loader from "./Loader";
 import curiosity from "../images/curiosity.jpg";
 import mars1 from "../images/mars1.jpg";
 import mars2 from "../images/mars2.jpg";
 import mars3 from "../images/mars3.jpg";
-// import axios from "axios";
 
 class Slider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       photos: [curiosity, mars1, mars2, mars3],
-      loading: false,
-      index: 2,
+      index: 0,
     };
   }
 
   nextStep = () => {
-    if (this.index < this.photos.lenth) {
-      this.index = this.index + 1;
+    if (this.state.index < this.state.photos.length - 1) {
+      this.setState({
+        ...this.state,
+        index: this.state.index + 1,
+      });
     } else {
-      this.index = 0;
+      this.setState({
+        ...this.state,
+        index: 0,
+      });
     }
   };
 
   prevStep = () => {
     if (this.state.index !== 0) {
-      this.index = this.index - 1;
+      this.setState({
+        ...this.state,
+        index: this.state.index - 1,
+      });
     } else {
-      this.index = this.photos.lenth;
+      this.setState({
+        ...this.state,
+        index: this.state.photos.length - 1,
+      });
     }
   };
-
-  // componentWillMount() {
-  //   this.fetchPhotos();
-  // }
-
-  // fetchPhotos() {
-  //   const apiUrl =
-  //     "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY";
-  //   axios.get(apiUrl).then((response) => {
-  //     this.setState({
-  //       photos: response.data,
-  //       loading: false,
-  //     });
-  //   });
-  // }
 
   render() {
     return (
       <div id="slider">
-        <NexsStep direction="left" onClick={this.nextStep} />
-        {(this.state.loading && <Loader />) || (
-          <SliderStep
-            photo={this.state.photos[this.state.index]}
-          />
-        )}
-        <NexsStep direction="right" onClick={this.prevStep} />
+        <button onClick={this.prevStep}>
+          <IconStep direction="left" />
+        </button>
+        <SliderStep photo={this.state.photos[this.state.index]} />
+        <button onClick={this.nextStep}>
+          <IconStep direction="right" />
+        </button>
       </div>
     );
   }
