014dee6706d576d6495ee0a030aad854a0fa4a6d
Dario Di Gulio <ddigulio@technisys.com>
2020-07-06 12:46:00 -0300

    Updates

---
+++ precache-manifest.1286c437ba5059323dd6e10a17af6878.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606d5af6fa7a24c01f2c1b30b0271bdc",
+    "url": "/Rover-Curiosity-on-Mars/index.html"
+  },
+  {
+    "revision": "e990345fcffc4221bf92",
+    "url": "/Rover-Curiosity-on-Mars/static/css/main.bc08dd2e.chunk.css"
+  },
+  {
+    "revision": "7ac6b3ffafbfae699419",
+    "url": "/Rover-Curiosity-on-Mars/static/js/2.d8346981.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rover-Curiosity-on-Mars/static/js/2.d8346981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e990345fcffc4221bf92",
+    "url": "/Rover-Curiosity-on-Mars/static/js/main.c1b338bc.chunk.js"
+  },
+  {
+    "revision": "5540a249e93bcb2cd551",
+    "url": "/Rover-Curiosity-on-Mars/static/js/runtime-main.77058d45.js"
+  },
+  {
+    "revision": "fb9afd32dbc7e13d5de0b5781fe29181",
+    "url": "/Rover-Curiosity-on-Mars/static/media/curiosity.fb9afd32.jpg"
+  },
+  {
+    "revision": "eb9438ebbddf72919a4d6d335addb0e6",
+    "url": "/Rover-Curiosity-on-Mars/static/media/mars.eb9438eb.jpeg"
+  },
+  {
+    "revision": "f32e402da5d7e54b8ace42f4d804df1a",
+    "url": "/Rover-Curiosity-on-Mars/static/media/mars1.f32e402d.jpg"
+  },
+  {
+    "revision": "c6986be187aa1ced52df2869df7c43f3",
+    "url": "/Rover-Curiosity-on-Mars/static/media/mars2.c6986be1.jpg"
+  },
+  {
+    "revision": "d818d61462d14b277b049700edf26d48",
+    "url": "/Rover-Curiosity-on-Mars/static/media/mars3.d818d614.jpg"
+  },
+  {
+    "revision": "cab24567c987d58d0b83e7bdba3f6a95",
+    "url": "/Rover-Curiosity-on-Mars/static/media/mars4.cab24567.jpg"
+  }
+]);
